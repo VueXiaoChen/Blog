5d2e09e3e883a39bbd8fe6c1571678d7f6fa5cf9
”昨前进明“ <455664949@qq.com>
2023-07-30 20:38:33 +0800

    增加启动成功提示和显示端口!

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -1,13 +1,25 @@
 package com.example.blog;
 
+import com.example.blog.websocket.WebSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.xml.stream.events.EndElement;
+import java.util.EventListener;
 
 @SpringBootApplication
+@EnableScheduling
 public class BlogApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(WebSocketServer.class);
     public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
+        SpringApplication app = new SpringApplication(BlogApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
