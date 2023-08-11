c146805ff08d04d6c6365dedc73e93ab8f6d307d
昨前今明 <455664949@qq.com>
2023-08-11 23:30:49 +0800

    blog增加初始富文本框并完成创建功能

--- web/src/views/Blog/addindex.vue
+++ web/src/views/Blog/addindex.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
-import { ref, watch, reactive,onMounted } from "vue"
+import { ref, watch, reactive,onMounted,shallowRef,onBeforeUnmount } from "vue"
 import {type FormInstance,FormRules,ElMessage } from 'element-plus'
 import { GetBlogTypeApi,GetBlogTagApi,GetBlogUpdateOrAddApi } from "@/api/blog/index"
 import { useUserStore } from "@/store/modules/user"
+import '@wangeditor/editor/dist/css/style.css'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+
 const loading = ref<boolean>(false)
 const user = useUserStore()
 const formSize = ref('default')
@@ -36,6 +39,27 @@ const options = Array.from({ length: 10000 }).map((_, idx) => ({
   value: `${idx + 1}`,
   label: `${idx + 1}`,
 }))
+
+
+
+
+// 编辑器实例，必须用 shallowRef
+    const editorRef = shallowRef()
+    // 内容 HTML
+    const valueHtml = ref('<p>请输入内容...</p>')
+    const toolbarConfig = {}
+    const editorConfig = { placeholder: '请输入内容...' }
+    // 组件销毁时，也及时销毁编辑器
+    onBeforeUnmount(() => {
+        const editor = editorRef.value
+        if (editor == null) return
+        editor.destroy()
+    })
+    const handleCreated = (editor) => {
+      editorRef.value = editor // 记录 editor 实例，重要！
+    }
+    const mode = ref('default')
+
 
 
 /** 博客类型获取 */
@@ -87,6 +111,9 @@ const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
     if (valid) {
+      ruleForm.blogContent = valueHtml.value
+      console.log(ruleForm.blogContent);
+      
       GetBlogUpdateOrAdd(ruleForm)
     } else {
       console.log('error submit!', fields)
@@ -104,6 +131,7 @@ function BlogCheckRadioChange(item){
 onMounted(() => {
   GetBlogType()
   GetBlogTag()
+  //valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
 });
 
 </script>
@@ -137,7 +165,21 @@ onMounted(() => {
           </el-checkbox-group>
         </el-form-item>
         <el-form-item label="文章内容：" prop="blogContent">
-          <el-input v-model="ruleForm.blogContent" type="textarea" />
+          <div style="border: 1px solid #ccc">
+            <Toolbar
+              style="border-bottom: 1px solid #ccc"
+              :editor="editorRef"
+              :defaultConfig="toolbarConfig"
+              :mode="mode"
+            />
+            <Editor
+              style="height: 500px; overflow-y: hidden;"
+              v-model="valueHtml"
+              :defaultConfig="editorConfig"
+              :mode="mode"
+              @onCreated="handleCreated"
+            />
+          </div>
         </el-form-item> 
         <el-form-item>
           <el-button type="primary" @click="submitForm(ruleFormRef)">创建</el-button>
